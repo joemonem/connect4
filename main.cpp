34605b0afb5b88a431eff5f5f655c0508255ff47
Joe Monem <jmonem@icloud.com>
2023-11-12 13:39:01 +0200

    added vertical check for winner

--- main.cpp
+++ main.cpp
@@ -60,23 +60,23 @@ int main()
             }
         }
 
-        // // Let's test what we have so far
+        // Let's test what we have so far
 
-        // for (int i = 0; i < 6; i++)
-        // {
-        //     for (int j = 0; j < 7; j++)
-        //     {
-        //         if (board[i][j].empty())
-        //         {
-        //             cout << "E";
-        //         }
-        //         else
-        //         {
-        //             cout << board[i][j];
-        //         }
-        //     }
-        //     cout << '\n';
-        // }
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 7; j++)
+            {
+                if (board[i][j].empty())
+                {
+                    cout << "E";
+                }
+                else
+                {
+                    cout << board[i][j];
+                }
+            }
+            cout << '\n';
+        }
 
         // TODO Check for a winner
 
@@ -110,7 +110,35 @@ int main()
             }
         }
 
-        // Vertical check
+        // Vertical check, top loop represents index
+        for (int i = 0; i < 7; i++)
+        {
+            for (int x = 5; x >= 3; x--)
+            {
+                if (board[x][i] == "E")
+                {
+                    continue;
+                }
+                // collect array of 4 consecutive elements
+                array<string, 4> sub;
+                for (int j = 0; j < 4; j++)
+                {
+                    sub[j] = board[x - j][i];
+                }
+
+                // Compare the 4 consecutive elements beginning from index j with the winning combinations
+                if (equal(sub.begin(), sub.end(), begin(winningComination1)))
+                {
+                    verdict = "Player 1 Wins!";
+                    break;
+                }
+                else if (equal(sub.begin(), sub.end(), begin(winningComination2)))
+                {
+                    verdict = "Player 2 Wins!";
+                    break;
+                }
+            }
+        }
 
         // Diagonal check
     }
